40cd44c8f3d7893773fe900feb01f81d80fa93aa
hasti <hasti723@gmail.com>
2019-03-23 10:39:31 +0530

    Method added

--- V01_InnovtionWebAPP/Models/enquiryModel.cs
+++ V01_InnovtionWebAPP/Models/enquiryModel.cs
@@ -13,6 +13,12 @@ namespace V01_InnovtionWebAPP.Model
         public string email { get; set; }
         public string contactNumber { get; set; }
         public string queryMessage { get; set; }
-        public string createdOn { get; set; }
+        public string response { get; set; }
+        public Boolean respondedUsing { get; set; }
+        public string comments { get; set; }
+        public DateTime createdOn { get; set; }
+        public int updatedBy { get; set; }
+        public DateTime updatedOn { get; set; }
+        
     }
 }
